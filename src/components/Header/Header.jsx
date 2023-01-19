b8d5d48442e3036ba492fda6d086682beeb0cb4f
Mohd Rejoan <wearecrx@gmail.com>
2023-01-19 16:34:02 +0600

    header complete for large screen

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,33 +14,31 @@ const Header = () => {
                     </a>
                 </div>
             </nav> */}
-            <Navbar className=" px-4 d-flex justify-content-between align-items-center ">
-                <div className=" brand_logo">
-                    <img
-                        className=" w-100 "
-                        src={BrandLogo}
-                        alt="Brand Logo"
-                    />
+            <Navbar className=" px-4  ">
+                <div className=" w-100 d-flex justify-content-between align-items-center py-lg-4 max-container ">
+                    <div className=" brand_logo d-lg-none ">
+                        <img
+                            className=" w-100 "
+                            src={BrandLogo}
+                            alt="Brand Logo"
+                        />
+                    </div>
+                    <NavbarBrand className=" brand-title-logo me-0 ">
+                        <img className=" w-100" src={Logo} alt="Brand Logo" />
+                    </NavbarBrand>
+                    {/* navbar  */}
+                    <Nav className=" d-none d-lg-flex">
+                        {["home", "new metting", "account"].map((link) => {
+                            return (
+                                <NavItem>
+                                    <NavLink className=" text-capitalize ">
+                                        {link}
+                                    </NavLink>
+                                </NavItem>
+                            );
+                        })}
+                    </Nav>
                 </div>
-                <NavbarBrand className=" me-0 ">
-                    <img
-                        className=" w-100"
-                        src={Logo}
-                        alt="Brand Logo"
-                    />
-                </NavbarBrand>
-                {/* navbar  */}
-                <Nav className=" d-none">
-                    {["home", "new metting", "account"].map((link) => {
-                        return (
-                            <NavItem>
-                                <NavLink className=" text-capitalize ">
-                                    {link}
-                                </NavLink>
-                            </NavItem>
-                        );
-                    })}
-                </Nav>
             </Navbar>
         </header>
     );
