aad30eff033dfba7172fae37f842d70452bec8ec
Mohd Rejoan <wearecrx@gmail.com>
2023-01-20 19:05:21 +0600

    submit btn added

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Form, FormControl } from "react-bootstrap";
 import FormLogo from "../assets/overlap-name.png";
-import Style from "./scss/SignUpPage.module.scss";
+import "./scss/SignUpPage.scss";
 import GoogleBtn from "../components/Ui/WithGoogleBtn/GoogleBtn";
+import { CgCornerDownRight } from "react-icons/cg";
 
 const SignUpPage = () => {
     return (
@@ -15,10 +16,10 @@ const SignUpPage = () => {
                 <GoogleBtn />
                 {/* or text  */}
                 <p className=" text-16px my-3 text-center ">or</p>
-                <Form className={`${Style.form}`}>
+                <Form className={``}>
                     <Form.Group>
                         <Form.Control
-                            className=" round-element bg-light-gray-btn border-0 mb-2"
+                            className=" round-element bg-light-gray-btn border-0 mb-3"
                             type="email"
                             placeholder="Email"
                         />
@@ -27,6 +28,18 @@ const SignUpPage = () => {
                             type="password"
                             placeholder="Password"
                         />
+                    </Form.Group>
+                    <Form.Group className=" submit-wrap d-flex justify-content-between align-items-center px-3">
+                        <a href="" className=" text-light-gray text-12px ">
+                            Forgot password?
+                        </a>
+                        <button
+                            type="submit"
+                            className={`round-element cus-btn submitBtn`}
+                        >
+                            Log in{" "}
+                            <CgCornerDownRight style={{ strokeWidth: "1px" }} />
+                        </button>
                     </Form.Group>
                 </Form>
             </div>
