d764be769292ee381e50d655b464aaeb023f032c
Mohd Rejoan <wearecrx@gmail.com>
2023-01-20 13:04:49 +0600

    form input created

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form } from "react-bootstrap";
+import { Form, FormControl } from "react-bootstrap";
 import FormLogo from "../assets/overlap-name.png";
 import Style from "./scss/SignUpPage.module.scss";
 import GoogleBtn from "../components/Ui/WithGoogleBtn/GoogleBtn";
@@ -13,8 +13,21 @@ const SignUpPage = () => {
                 </legend>
                 {/* google button  */}
                 <GoogleBtn />
+                {/* or text  */}
+                <p className=" text-16px my-3 text-center ">or</p>
                 <Form className={`${Style.form}`}>
-                    <Form.Group></Form.Group>
+                    <Form.Group>
+                        <Form.Control
+                            className=" round-element bg-light-gray-btn border-0 mb-2"
+                            type="email"
+                            placeholder="Email"
+                        />
+                        <Form.Control
+                            className=" round-element bg-light-gray-btn border-0  "
+                            type="password"
+                            placeholder="Password"
+                        />
+                    </Form.Group>
                 </Form>
             </div>
         </div>
