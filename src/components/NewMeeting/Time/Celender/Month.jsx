a85e69baa551d760575572f551e14f404ab1d57c
Mohd Rejoan <wearecrx@gmail.com>
2023-01-22 19:02:46 +0600

    year month data list generantor created

--- src/components/NewMeeting/Time/Celender/Month.jsx
+++ src/components/NewMeeting/Time/Celender/Month.jsx
@@ -2,41 +2,44 @@ import React, { useState, useEffect } from "react";
 import moment from "moment/moment";
 
 const Month = () => {
-    const [monthClenderDetails, setMonthClenderDetails] = useState();
+    const [yearDetails, setYearDetails] = useState({});
     const [monthList, setMonthList] = useState([]);
 
     // set month list
     useEffect(() => {
         // generate months
-        const months = moment.months();
+        const months = moment.months({});
         setMonthList(() => {
             return [...months];
         });
         /// generate months day list
-        // monthList.map((month, monthIndex) => {
-        //     var monthIndex = a;
-        //     var names = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
-        //     var date = new Date(2023, monthIndex, 1);
-        //     var result = [];
-        //     while (date.getMonth() == monthIndex) {
-        //         result.push(date.getDate() + "-" + names[date.getDay()]);
-        //         date.setDate(date.getDate() + 1);
-        //     }
-        //     // console.log(result);
-        // });
+        monthList.map((monthName, monthIndex) => {
+            var names = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+            var date = new Date(2023, monthIndex, 1);
+            var result = [];
+            while (date.getMonth() == monthIndex) {
+                result.push({
+                    date: date.getDate(),
+                    name: names[date.getDay()],
+                });
+                date.setDate(date.getDate() + 1);
+            }
+            // console.log(result);
+            // set year details
+            setYearDetails((prev) => {
+                const copyMonth = { ...prev };
+                copyMonth[monthName.toLowerCase()] = [...result];
+                return copyMonth;
+            });
+        });
     }, []);
+  
+  
+  console.log(moment().week(4))
 
-    // for (let a = 0; a <= 11; a++) {
-    //     var monthIndex = a;
-    //     var names = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
-    //     var date = new Date(2023, monthIndex, 1);
-    //     var result = [];
-    //     while (date.getMonth() == monthIndex) {
-    //         result.push(date.getDate() + "-" + names[date.getDay()]);
-    //         date.setDate(date.getDate() + 1);
-    //     }
-    //     // console.log(result);
-    // }
+    useEffect(() => {
+        console.log(yearDetails);
+    }, [yearDetails]);
 
     return (
         <div>
